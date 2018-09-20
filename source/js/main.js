574a9e2bc2f58c6104ef00c56a89a951cc0af083
kassiopea <fotograf.anastasia@gmail.com>
2018-09-20 16:36:36 +0300

    добавлен левый фиксированный блок

--- source/js/main.js
+++ source/js/main.js
@@ -6,7 +6,7 @@ var jobsDate = document.querySelectorAll(".jobs__wrap-dates");
 //     }
 // }
 
-if (window.matchMedia("(min-width: 768px) and (max-width: 1023px)")) {
+if (window.matchMedia("(min-width: 768px) and (max-width: 1023px)").matches) {
     for (let i = 0; i < jobsDate.length; i +=2){
         jobsDate[i].classList.add("jobs__dates-line");
     }
