8fd07681f2573813ba4a0af2e07264594b7aa69e
kassiopea <fotograf.anastasia@gmail.com>
2018-09-28 13:04:29 +0300

    добавлены ссылки на соцсети в левом блоке на десктопе

--- source/js/main.js
+++ source/js/main.js
@@ -1,12 +1,12 @@
 var jobsDate = document.querySelectorAll(".jobs__wrap-dates");
 
-// if (window.matchMedia("(min-width: 768px) and (max-width: 1023px)")) {
-//     for (let i = 0; i < jobsDate.length; i+2){
+// if (window.matchMedia("(min-width: 768px) and (max-width: 1023px)").matches) {
+//     for (let i = 0; i < jobsDate.length; i +=2){
 //         jobsDate[i].classList.add("jobs__dates-line");
 //     }
 // }
 
-if (window.matchMedia("(min-width: 768px) and (max-width: 1023px)").matches) {
+if (window.matchMedia("(min-width: 768px) and (max-width: 1200px)").matches) {
     for (let i = 0; i < jobsDate.length; i +=2){
         jobsDate[i].classList.add("jobs__dates-line");
     }
