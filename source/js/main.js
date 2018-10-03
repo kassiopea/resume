ba416d02b0f1723ed250be31a415b826435416bf
kassiopea <fotograf.anastasia@gmail.com>
2018-10-03 15:29:29 +0300

    при разрешении выше 1200, блок в одну строку, плюс добавлены стили линии между блочками, при элементах в двух колонках и  в одну

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,12 @@
 var jobsDate = document.querySelectorAll(".education__wrap-dates");
 
-if (window.matchMedia("(min-width: 600px) and (max-width: 1200px)").matches) {
+if (window.matchMedia("(min-width: 600px) and (max-width: 1199px)").matches) {
     for (let i = 0; i < jobsDate.length; i +=2){
+        jobsDate[i].classList.add("education__dates-line");
+    }
+}else if(window.matchMedia("(min-width: 1200px)").matches){
+    for (let i = 0; i < jobsDate.length - 1; i ++){
+        console.log(jobsDate[i]);
         jobsDate[i].classList.add("education__dates-line");
     }
 }
