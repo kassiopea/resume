d4468c9664217a7f96c4f7598c34e0b0a14ad0f0
kassiopea <fotograf.anastasia@gmail.com>
2018-10-03 12:07:56 +0300

    исправлен баг появления горизонтальной прокрутки у блока с опытом работы при ширине выше 1200пикс. Замена ширины блока в 100% на ширину в пикселях и ограничение макс.ширины в 1200пикс при ширине экрана выше 1600пикс

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,6 @@
 var jobsDate = document.querySelectorAll(".education__wrap-dates");
 
-if (window.matchMedia("(min-width: 768px) and (max-width: 1200px)").matches) {
+if (window.matchMedia("(min-width: 600px) and (max-width: 1200px)").matches) {
     for (let i = 0; i < jobsDate.length; i +=2){
         jobsDate[i].classList.add("education__dates-line");
     }
